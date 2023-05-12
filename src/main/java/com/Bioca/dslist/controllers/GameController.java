5b571d2261651c7a530d04ed83670cdec038059b
Carlos Henrique <101880890+Nunes-01@users.noreply.github.com>
2023-05-12 00:58:53 -0300

    Get game by  id

--- src/main/java/com/Bioca/dslist/controllers/GameController.java
+++ src/main/java/com/Bioca/dslist/controllers/GameController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Bioca.dslist.dto.GameDTO;
 import com.Bioca.dslist.dto.GameMinDTO;
-import com.Bioca.dslist.entities.Game;
 import com.Bioca.dslist.services.GameService;
 
 @RestController
@@ -17,6 +18,13 @@ public class GameController {
 
 	@Autowired
 	private GameService gameservice;
+
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable long id) {
+		GameDTO result = gameservice.findById(id);
+		return result;
+
+	}
 
 	@GetMapping
 	public List<GameMinDTO> findAll() {
--- src/main/java/com/Bioca/dslist/services/GameService.java
+++ src/main/java/com/Bioca/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.Bioca.dslist.dto.GameDTO;
 import com.Bioca.dslist.dto.GameMinDTO;
 import com.Bioca.dslist.entities.Game;
 import com.Bioca.dslist.repositories.Gamepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private Gamepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
